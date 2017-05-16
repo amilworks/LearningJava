0932185080b485481ce46d2a86b6afb35f955f49
Amil Khan <amil@umail.ucsb.edu>
2017-05-16 15:35:47 -0700

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,17 @@
+/**
+ * Standard "Hello World"
+ * A lot more code than Python
+ * So whatever you name the class is what you need to name the file, "helloworld.java"
+ */
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
+/**
+ * First time I ran this, I got this error
+ * Error:(7, 42) java: ';' expected
+ * Better than R already
+ */
